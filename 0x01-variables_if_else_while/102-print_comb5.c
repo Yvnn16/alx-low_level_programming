017124ff56cf1ac375597e252370242ac4b9fb5e
Yvnn16 <yvonneweru5@gmail.com>
2022-09-09 05:40:51 -0700

    Extra

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,22 +7,20 @@
  */
 int main(void)
 {
-int c, i;
+int digit1, digit2;
 
-c = i = '0';
+for (digit1 = 0; digit1 < 10; digit1++)
+{
+for (digit2 = 0; digit2 < 10; digit2++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
-for (c = '0'; c <= '9'; c++)
-{
-for (i = '0'; i <= '9'; i++)
-{
-putchar(c);
-putchar(i);
+if (digit1 == 9 && digit2 == 9)
+continue;
 
-if ((c != '9') || (c == '9' && i != '9'))
-{
 putchar(',');
 putchar(' ');
-}
 }
 }
 
