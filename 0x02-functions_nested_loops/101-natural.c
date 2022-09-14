f097ed1f779686624afbdef33fec22e1e4cfe6a5
Yvnn16 <yvonneweru5@gmail.com>
2022-09-14 07:30:55 -0700

    Task 0

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * prints count of multiples of 3 or 5 below 1024
+ * main - main block
+ * Description: Prints count of multiples of 3 or 5 below 1024
  *  Return: 0
  */
 int main(void)
