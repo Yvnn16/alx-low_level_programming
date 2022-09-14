7045818df65297d1bc87eaa057998cd842e1e0a5
Yvnn16 <yvonneweru5@gmail.com>
2022-09-14 08:02:16 -0700

    Extra

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,6 +13,7 @@ unsigned long m, n, p, carry;
 
 count = 0;
 i = 0;
+amount = 100;/*10^2*/
 for (count = 1; count <= 91; count++)
 {
 k = i + j;
@@ -20,15 +21,15 @@ i = j;
 j = k;
 printf("%lu, ", k);
 }
-m = i % 1000;
-i = i / 1000;
-n = j % 1000;
-j = j / 1000;
+m = i % amount;
+i = i / amount;
+n = j % amount;
+j = j / amount;
 while (count <= 98)
 {
-carry = (m + n) / 1000;
-p = (m + n) - carry * 1000;
-k = (i + j) + carry;
+carry = (m + n) / amount;
+p = (m + n) -carry * amount;
+k = (i + j) +carry;
 m = n;
 n = p;
 i = j;
