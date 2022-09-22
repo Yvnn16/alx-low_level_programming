16a97563c43e9c6a64c5bfb19315d5ecef7e531c
Yvnn16 <yvonneweru5@gmail.com>
2022-09-22 02:54:27 -0700

    Tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,23 +13,23 @@ int index = 0;
 while (str[index])
 {
 while (!(str[index] >= 'a' && str[index] <= 'z'))
-index++;
+	index++;
 
-if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}' ||
-index == 0)
-str[index] -= 32;
+if (str[index - 1] == ' ' || 
+    str[index - 1] == '\t' ||
+    str[index - 1] == '\n' ||
+    str[index - 1] == ',' ||
+    str[index - 1] == ';' ||
+    str[index - 1] == '.' ||
+    str[index - 1] == '!' ||
+    str[index - 1] == '?' ||
+    str[index - 1] == '"' ||
+    str[index - 1] == '(' ||
+    str[index - 1] == ')' ||
+    str[index - 1] == '{' ||
+    str[index - 1] == '}' ||
+    index == 0)
+	str[index] -= 32;
 
 index++;
 }
